88bdf2c196d77659874c6ee46f61cc4949297548
Ayush Mangal <mangal.ayush.4982@gmail.com>
2023-10-22 05:13:33 +0530

    completed design

---
+++ app/components/Table.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Table = () => {
+  return (
+    <div className='w-full bg-gradient-to-r from-[#121212] to-[#190034] text-white rounded-md p-3 flex items-center justify-center'>
+        table
+    </div>
+  )
+}
+
+export default Table
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
 'use client'
 import { useAccountAbstraction } from '@/context/accountAbstractionContext'
+import Table from './components/Table';
+import { useState } from 'react';
+import { Inter,Jura } from 'next/font/google';
+import {fadeIn} from '../variants'
+import {motion } from 'framer-motion'
+
+const jura = Jura({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  style: ["normal"],
+});
 
 export default function Home() {
   const {
@@ -8,17 +19,49 @@ export default function Home() {
     ownerAddress // Add this userAddress state variable
   } = useAccountAbstraction();
 
+  const [contractAddress, setContractAddress] = useState('')
+
   return (
-    <div className={`flex ${isAuthenticated ? 'mt-5':'items-center h-screen'} justify-center transition-all duration-300`}>
-      <button className='lgShadow bg-gradient-to-r from-[#121212] to-[#190034] text-white rounded-xl px-5 py-3' onClick={loginWeb3Auth}>
+      <div className={`flex min-h-screen ${isAuthenticated ? 'mt-10':'items-center h-screen'} justify-center transition-all duration-300 textSize font-bold ${jura.className}`}>
+      
       {!isAuthenticated ? (
-        <p>Connect Wallet</p>
+        <motion.div
+          variants={fadeIn('up',0.2)}
+          initial='hidden'
+          animate='show'
+          exit='hidden'
+        >
+          <button className='lgShadow bg-gradient-to-r from-[#121212] to-[#190034] text-white rounded-md p-3' 
+          onClick={loginWeb3Auth}
+        >
+          Connect Wallet
+        </button>
+        </motion.div>
         ) : (
-        <p className=''>
-          {ownerAddress.slice(0,5)}.....{ownerAddress.slice(38,44)}
-        </p>
+          <div className='flex flex-col gap-y-8 w-[80%] lg:w-4/5 items-center '>
+            <motion.div
+              variants={fadeIn('down',0.2)}
+              initial='hidden'
+              animate='show'
+              exit='hidden'  
+              className='flex flex-col md:flex-row items-center justify-between w-[80%] lg:w-4/5 gap-5'
+            >
+              <input className='text-[#121212] z-10 bg-white/80 p-3 w-full rounded-md outline-none justify-between' type='text' value={contractAddress} placeholder='Contract Address...' onChange={(e)=> setContractAddress(e.target.value)}/>
+              <button className='lgShadow bg-gradient-to-r w-full from-[#121212] to-[#190034] text-white rounded-md p-3' onClick={loginWeb3Auth}>
+                {ownerAddress.slice(0,5)}.....{ownerAddress.slice(38,44)}
+              </button>
+            </motion.div>
+            <motion.div 
+              variants={fadeIn('up',0.4)}
+              initial='hidden'
+              animate='show'
+              exit='hidden' 
+              className='lgShadow w-[80%] lg:w-4/5 bg-gradient-to-r from-[#121212] to-[#190034] text-white rounded-md flex items-center justify-center'
+              >
+                <Table/>
+            </motion.div>
+          </div>
         )}
-      </button>
     </div>
   );
 }
